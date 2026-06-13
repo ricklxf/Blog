d46581e93cfdd087a51d267b3c7dcfa41ead434f
ricklxf <ricklxf@icloud.com>
2026-06-14 00:00:07 +0800

    feat: 初始化个人博客

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'My Blog';
+export const SITE_DESCRIPTION = '个人博客，记录技术与思考。';
